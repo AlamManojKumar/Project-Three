1992de0b9f73d3fc0de672817bcd9ae40d393eb1
Manoj <manojkumaralamd365@gmail.com>
2026-03-15 11:53:32 +0530

    git add .

---
+++ PythonBasicPrograms.py/Tuple.py
@@ -0,0 +1,11 @@
+#tuple
+tuple =(1,2,3,4,5,6,7,8)
+for tup in range (1,8):
+    print(tup)
+#List
+variable =[1,"manoj","mahesh",2,4,5,6]
+for item in variable:
+    print(item)
+
+for index in range(1,10):
+    print(index)
